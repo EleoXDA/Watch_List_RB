3a7a9eade398ac0d920f6c72b84632642f5624f4
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-25 08:28:53 +0200

    ADD DB seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,15 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Movie.destroy_all
+
+puts 'Creating movies...'
+100.times do |n|
+  Movie.create!(title: Faker::Movie.title, overview: Faker::Quote.famous_last_word,
+                poster_url: Faker::LoremFlickr.image(size: '300x300', search_terms: ['movies'], match_all: true),
+                rating: rand(0..10))
+end
+puts 'Finished!'
