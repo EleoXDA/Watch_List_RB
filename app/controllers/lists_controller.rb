de67dc06e46d126c2785e36957ee7f5f93ba54e3
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-25 13:46:15 +0200

    ADD: Controller for Lists new

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,4 +7,17 @@ class ListsController < ApplicationController
     id = params[:id]
     @list = List.find(id)
   end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
