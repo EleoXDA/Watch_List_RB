f9f458c6bd57a656374692f4020ef41350b99ce4
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-26 15:23:26 +0200

    FIX: inability to select movie in bookmarks window

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -34,6 +34,6 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
