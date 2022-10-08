2e67358184194e7d24b75310515f0c3e591c65a4
Elbay Malikmammadov <eleo.666@gmail.com>
2022-10-08 23:33:13 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: 'movies#index'
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :movies, only: %i[show index] do # add show and index
-    # nested routes for bookmarks
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "lists#index"
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
   end
-
-  resources :lists, only: %i[show index new create] do # add show index new create
-    # nested routes for bookmarks
-    resources :bookmarks, only: %i[index new create] # add new create
-  end
-
-  resources :bookmarks, only: %i[show destroy]
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
-
-# root to: 'tasks#index'
-#   get 'tasks', to: 'tasks#index', as: :tasks
-#   post 'tasks', to: 'tasks#create'
-#   get 'tasks/new', to: 'tasks#new', as: :new_task
-#   get 'tasks/:id', to: 'tasks#show', as: :task
-#   patch 'tasks/:id', to: 'tasks#update'
-#   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-#   delete 'tasks/:id', to: 'tasks#destroy'
